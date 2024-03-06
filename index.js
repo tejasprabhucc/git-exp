880f3e06574a6479cabe4556bd3ec153e38f100d
tejas <tejas.prabhu@codecraft.co.in>
2024-03-06 16:15:05 +0530

    in main after merge

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ console.log("hello");
  * @param {number} a 
  * @param {number} b 
  * @returns {number}
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
  */
 function add(a,b){
     return a+b;
 }
 const sum = add(10, 15);
+
+function subtract(a,b){
+    return a-b;
+}
+const diff = subtract(100, 15);
 
 function multiply(a,b){
     return a*b;
