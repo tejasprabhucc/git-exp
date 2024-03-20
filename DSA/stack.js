b5a49604425d456c247e5547abadd8077a039ade
tejas <tejas.prabhu@codecraft.co.in>
2024-03-20 14:28:29 +0530

    Documented

--- DSA/stack.js
+++ DSA/stack.js
@@ -1,11 +1,18 @@
 //Implementing stack without class 
 const stack = [];
 
+/**
+ * 
+ * @param {element} element 
+ */
 function push(element){
     stack.push(element);
 }
  
-
+/**
+ * 
+ * @returns top element of stack DS
+ */
 function pop(){
     return stack.pop();
 }
